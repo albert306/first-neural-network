9e55400a97d47a05cade689c4c52127f08392fe4
albert306 <a.wolf04@protonmail.com>
2024-03-20 15:07:13 +0100

    added loss calculating functions

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "Layer.h"
+#include "DataPoint.h"
 
 using namespace std;
 
@@ -24,7 +25,7 @@ class NeuralNetwork {
 
         int classify(vector<double> inputs) {
             vector<double> outputs = this->calculateOutputs(inputs);
-            
+
             // find nodeindex with highest activation
             double highestActivation = -INFINITY;
             int highestActivationIndex = 0;
@@ -35,5 +36,32 @@ class NeuralNetwork {
                 }
             }
             return highestActivationIndex;
+        }
+
+        double nodeLoss(double activation, double expected) {
+            double error = expected - activation;
+            return error * error;
+        }
+
+        double loss(DataPoint dataPoint) {
+            vector<double> outputs = this->calculateOutputs(dataPoint.inputs);
+            
+            double loss = 0;
+            // calculate loss for each node and add up
+            for (int i = 0; i < outputs.size(); i++) {
+                loss += nodeLoss(outputs[i], dataPoint.expectedOutputs[i]);
+            }
+
+            return loss;
+        }
+
+        double loss(vector<DataPoint> data) {
+            double totalLoss = 0;
+
+            for (DataPoint dataPoint : data) {
+                totalLoss += loss(dataPoint);
+            }
+
+            return totalLoss / data.size();
         }
 };
